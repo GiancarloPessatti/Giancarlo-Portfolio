31748f52fa30cdb06894452e8e52c4fb3769e255
GiancarloPessatti <106692510+GiancarloPessatti@users.noreply.github.com>
2023-04-04 20:43:52 -0300

    feat: datauser

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Giancarlo Pessatti",
+  githubUser: "giancarlopessatti",
+  linkedinUser: "giancarlopessatti",
+  whatsappNumber: "+5547988793151",
 };
