2c24166910c92d3e62019bbba585ac6f2c3511e5
GiancarloPessatti <106692510+GiancarloPessatti@users.noreply.github.com>
2023-04-09 17:57:59 -0300

    feat: git fix

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
   nameUser: "Giancarlo Pessatti",
   githubUser: "giancarlopessatti",
-  linkedinUser: "giancarlopessatti",
+  linkedinUser: "giancarlo-pessatti-2b2641219",
   whatsappNumber: "+5547988793151",
 };
